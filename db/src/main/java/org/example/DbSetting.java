abcb8a3e96ab82784c919ce6b6bbbe833296c8fb
Sudarev <soul7341@gmail.com>
2023-10-29 21:42:59 +0300

    Создан многомодульный проект на Maven

---
+++ db/src/main/java/org/example/DbSetting.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class DbSetting {
+    public String name;
+    public String password;
+
+    public DbSetting(String name, String password){
+        this.name = name;
+        this.password = password;
+    }
+}
